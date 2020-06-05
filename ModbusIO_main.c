7f3296c700788fef6f755bf75b581a8f746f78dd
Hans Unzner <hansunzner@gmail.com>
2020-06-05 23:12:59 +0200

    Implemented moving average

--- ModbusIO_main.c
+++ ModbusIO_main.c
@@ -27,79 +27,111 @@
 #include <stdio.h>
 #include "modbus.h"
 
- /** Configuration ********************************************************/
-#pragma		config	OSC = INTIO67
-#pragma		config	PWRT = ON    	// power up timer on
-#pragma		config	BOREN = OFF   	// brown out detect
-#pragma		config	WDT = OFF    	// watchdog off
-#pragma		config	LVP = OFF    	// lvp off
-#pragma		config	FCMEN = OFF
-#pragma		config	IESO = OFF
-#pragma		config 	MCLRE = OFF
-#pragma		config	PBADEN = OFF
-#pragma		config  DEBUG = OFF
+ /** Configuration ********************************************************/ 
+#pragma  config  OSC = INTIO67
+#pragma  config  PWRT = ON      // power up timer on
+#pragma  config  BOREN = OFF    // brown out detect
+#pragma  config  WDT = OFF      // watchdog off
+#pragma  config  LVP = OFF      // lvp off
+#pragma  config  FCMEN = OFF
+#pragma  config  IESO = OFF
+#pragma  config  MCLRE = OFF
+#pragma  config  PBADEN = OFF
+#pragma  config  DEBUG = OFF
 
-#define N_ADC_CHANNELS	4
+#define N_ADC_CHANNELS 4
 #define BAUD19200
 
-unsigned int anADCValues_RAW[N_ADC_CHANNELS];
 
+typedef struct{
+  UINT16 anValuesAVG[16];
+  UINT8 nIndexAVG;
+  UINT32 nSum;
+} SMA_t;
+
+SMA_t astAVGValues[N_ADC_CHANNELS] = {{{0}}};
+UINT16 anADCValues_RAW[N_ADC_CHANNELS];
+
+UINT16 MovingAVG(SMA_t *pstValues, UINT8 nNumber, UINT16 nInput)
+{
+  pstValues->nSum -= pstValues->anValuesAVG[pstValues->nIndexAVG];
+  pstValues->anValuesAVG[pstValues->nIndexAVG] = nInput;
+  pstValues->nSum += nInput;
+    
+  if(pstValues->nIndexAVG < (nNumber-1))
+    pstValues->nIndexAVG++;
+  else
+    pstValues->nIndexAVG=0;
+  switch(nNumber){
+    case 4:
+      return pstValues->nSum >> 2;
+    case 8:
+      return pstValues->nSum >> 3;
+    case 16:
+      return pstValues->nSum >> 4;
+  }
+}
 
 #pragma code
 void main(void)
- {
-	const char anWelcomeString[] ={"ModbusIO starting...\r\n"};
-	OSCTUNEbits.PLLEN=1;	// PLL enable
-	OSCCONbits.IRCF0=1;		// 8 MHz clock
-	OSCCONbits.IRCF1=1;
-	OSCCONbits.IRCF2=1;
+{
+  const char anWelcomeString[] ={"ModbusIO starting...\r\n"};
+  OSCTUNEbits.PLLEN=1;  //PLL enable
+  OSCCONbits.IRCF0=1;    //8 MHz clock
+  OSCCONbits.IRCF1=1;
+  OSCCONbits.IRCF2=1;
 
-	TRISA = 0xFF;			// Set to input
-	TRISC = 0b10000000;	    // Set all to output except UART RX
-	TRISB = 0xFF;			// Set to input
+  TRISA = 0xFF;      // Set to input
+  TRISC = 0b10000000;      // Set all to output except UART RX
+  TRISB = 0xFF;      // Set to input
 
-//	ADCON2 = 0b10110101;	// right, 16 TAD, FOSC/16
-//	ADCON1 = 0b00001011;	// PortA AD I/O
-//	ADCON0 = 0b00001101;
+//  ADCON2 = 0b10110101;  //right, 16 TAD, FOSC/16
+//  ADCON1 = 0b00001011;  //PortA AD I/O
+//  ADCON0 = 0b00001101;
 
-	OpenPWM1(200);
-	OpenTimer2(T2_PS_1_4 & TIMER_INT_OFF);
-	SetDCPWM1(30);
-	RCONbits.IPEN = 0;			// Disable priority levels on interrupts
-	Nop();
-	INTCON = 0;
-	INTCONbits.GIE = 1;			// global interrupts enable
-	INTCONbits.PEIE = 1;		// global interrupts enable
-//	PIE2 = 0;
-//	PIE1bits.RCIE = 1;			// Enables the EUSART receive interrupt
+  OpenPWM1(200);
+  OpenTimer2(T2_PS_1_4 & TIMER_INT_OFF);
+  SetDCPWM1(30);
+  RCONbits.IPEN = 0;      //Disable priority levels on interrupts
+  Nop();
+  INTCON = 0;
+  INTCONbits.GIE = 1;      //global interrupts enable
+  INTCONbits.PEIE = 1;    //global interrupts enable
+//  PIE2 = 0;
+//  PIE1bits.RCIE = 1;      //Enables the EUSART receive interrupt
+  
 
-	// T0_PS_1_32 = 1.05 s
+  // T0_PS_1_32 = 1.05 s
 
-	OpenTimer0(TIMER_INT_ON & T0_16BIT & T0_SOURCE_INT & T0_PS_1_32);
-	OpenTimer1(TIMER_INT_ON & T1_16BIT_RW & T1_SOURCE_INT & T1_PS_1_2 & T1_OSC1EN_OFF);	
+  OpenTimer0(TIMER_INT_ON & T0_16BIT & T0_SOURCE_INT & T0_PS_1_32);
+  OpenTimer1(TIMER_INT_ON & T1_16BIT_RW & T1_SOURCE_INT & T1_PS_1_2 & T1_OSC1EN_OFF);  
 
-	Delay1KTCYx(100);		    // 50ms
+  Delay1KTCYx(100);    //50ms
 
 #ifdef BAUD19200
-	// Configure USART @19200 Baud 8N1 (spbrg = (f_osc/(16*Baudrate))-1)
-	OpenUSART( USART_TX_INT_OFF & USART_RX_INT_ON & USART_BRGH_HIGH & USART_ASYNCH_MODE & USART_EIGHT_BIT & USART_CONT_RX,	25);
+  // Configure USART @19200 Baud 8N1 (spbrg = (f_osc/(16*Baudrate))-1)
+  OpenUSART( USART_TX_INT_OFF & USART_RX_INT_ON & USART_BRGH_HIGH & USART_ASYNCH_MODE & USART_EIGHT_BIT & USART_CONT_RX,  25);
 #endif
- 	putsUSART ((char*)anWelcomeString);
+   putsUSART ((char*)anWelcomeString);
 
-	while(1)
-	{
-		OpenADC(ADC_FOSC_16 & ADC_RIGHT_JUST & ADC_16_TAD,
-				ADC_CH0 & ADC_INT_OFF & ADC_REF_VDD_VSS,
-				ADC_4ANA);
-		ConvertADC();	        // Start conversion
-		while(BusyADC());
-		anModbus_HoldingRegister[0] = ReadADC();
-		anModbus_HoldingRegister[1] = 2000;
-		anModbus_HoldingRegister[2] = 3000;
-		anModbus_HoldingRegister[3] = 4000;
+  while(1)
+  {
+    // Channel 0
+    OpenADC(ADC_FOSC_16 & ADC_RIGHT_JUST & ADC_16_TAD, ADC_CH0 & ADC_INT_OFF & ADC_REF_VDD_VSS, ADC_4ANA);
+    ConvertADC();
+    while(BusyADC());
+    anADCValues_RAW[0] = ReadADC();
+    // Channel 1
+    OpenADC(ADC_FOSC_16 & ADC_RIGHT_JUST & ADC_16_TAD, ADC_CH1 & ADC_INT_OFF & ADC_REF_VDD_VSS, ADC_4ANA);
+    ConvertADC();
+    while(BusyADC());
+    anADCValues_RAW[1] = ReadADC();
+    anModbus_HoldingRegister[0] = anADCValues_RAW[0]*10;
+    anModbus_HoldingRegister[1] = MovingAVG(&astAVGValues[0], 4, anADCValues_RAW[0]*10);
+    anModbus_HoldingRegister[2] = MovingAVG(&astAVGValues[1], 8, anADCValues_RAW[0]*10);
+    anModbus_HoldingRegister[3] = MovingAVG(&astAVGValues[2], 16, anADCValues_RAW[0]*10);
 
-		Delayms(10);
-		Delayx100ms(10);
-	}
+    Delayms(1);
+  }
 
 }
